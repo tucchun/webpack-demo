a7222c4092970ac02d7450d8be7f2e564480a928
tucchun <tucchun@hotmail.com>
2017-07-11 23:07:24 +0800

    Signed-off-by: tucchun <tucchun@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import _ from "lodash";
+import './style.css';
+import Icon from "./demo.jpg";
+
 function component() {
   var element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
 
+  element.classList.add("hello");
+
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
   return element;
 }
+
+
 document.body.appendChild(component());
